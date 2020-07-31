a2ab5337e2b06f7094d3dbb64392238464c93441
nirajkumar27297 <60932846+nirajkumar27297@users.noreply.github.com>
2020-08-01 01:50:09 +0530

    Added Java Codes for UserCase2

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,11 +1,23 @@
-
+import java.util.*;
 public class SnakeLadder {
-	
+	//Initialize Start Position as Zero
 	private int position = 0;
+	
+	//Function for rolling a dice
+	public int rollDice()
+	{
+		Random rand=new Random();
+		//generating Random Number from 0 to 5 and adding 1 as LowerLimit
+		int number=rand.nextInt(6)+1;
+		return number;
+	}
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SnakeLadder player1 = new SnakeLadder();
+		System.out.println(player1.rollDice());
 	}
 
 }
