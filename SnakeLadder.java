9becb62812c4b0ecb4dbf695863d014d6e870182
nirajkumar27297 <60932846+nirajkumar27297@users.noreply.github.com>
2020-08-02 22:00:56 +0530

    Refracting Java Codes for UserCase4

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,47 +4,45 @@ public class SnakeLadder {
 	private int position = 0;
 	
 	//Function for rolling a dice
-	private int rollDice()
-	{
-		Random rand=new Random();
+	private int rollDice() {
+		Random rand = new Random();
 		//generating Random Number from 0 to 5 and adding 1 as LowerLimit
-		int number=rand.nextInt(6)+1;
-		return number;
+		int resultDice = rand.nextInt(6) + 1;
+		return resultDice;
 	}
 	
 	//function for playing game
-	public void play()
-	{
+	public void play() {
 		System.out.println("Game Started");
-		
-		//Playing while position is less than 100
-		while(this.getPosition()<100)
-		{
-			//Rolling a dice
-			int number=this.rollDice();
-			System.out.println("The number on dice is "+number);
+		while(this.getPosition() < 100)
+		{   //Rolling a dice
+			int resultDice = this.rollDice();
+			System.out.println("The number on dice is "+resultDice);
 			//Getting position for the die 
-			int pos=this.getPosition();
+			int pos = this.getPosition();
 			//Calling checkOption function to generate Option	
-			int option=this.checkOption();
-			System.out.println("The option is "+option);
-			
-			//Ladder Position
-			if(option == 1)
+			int optionResult = this.checkOption();
+			switch(optionResult)
 			{
-				pos += number;
-				this.setPosition(pos);
-			}
-			//Snake Position
-			else if(option == 2)
-			{
-				pos -= number;
-				this.setPosition(pos);
+			    //Ladder Position
+				case 1 :
+					System.out.println("You got a Ladder");
+					pos += resultDice;
+					this.setPosition(pos);
+					break;
+				//Snake Position
+				case 2:
+					System.out.println("You got a Snake");
+					pos -= resultDice;
+					this.setPosition(pos);
+					break;
+				//No Play
+				default:
+					System.out.println("You got a NoPlay");
 			}
 			System.out.println("The position of the player is "+this.getPosition());
-			
 		}
-		
+			
 	}
 	
 	/**
@@ -61,26 +59,23 @@ public class SnakeLadder {
 		// If position < 0, then Position is assigned to zero
 		if(position < 0)
 		{
-			position=0;
+			position = 0;
 			
 		}
 		this.position = position;
 	}
-
-	public int checkOption()
-	{
-	     //check Option
-			/*
-			 * 0- No Play
-			 * 1-Ladder
-			 * 2-Snake
-			 */
-		
-		Random rand = new Random();
+	 //check Option
+	/*
+	 * 0- No Play
+	 * 1-Ladder
+	 * 2-Snake
+	 */
+	public int checkOption(){
+	    Random rand = new Random();
 		//Generating random numbers in range 0-2
-		int option=rand.nextInt(3);
+		int option = rand.nextInt(3);
 		return option;
-	}
+	} 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
